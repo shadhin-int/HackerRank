77e661bc7bd72e250fdd241fbbe1cf63625550f1
shadhin <shahinur.rahman@evaly.com.bd>
2021-10-12 17:49:46 +0600

    solve sock-merchant problem

---
+++ warm_up_challenge/sales_by_match.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+
+def sockMerchant(n, ar):
+    sum_of = 0
+    for value in Counter(ar).values():
+        sum_of += value//2
+    return sum_of
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
